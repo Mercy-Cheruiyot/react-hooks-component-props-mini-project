6a64093e76775e26ff5f309bdef56027a44e17cb
Mercy <mercycheruiyot2013@gmail.com>
2022-09-27 13:56:59 +0300

    Completed Assignment

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,11 +2,11 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({posts}){
-    const articles=posts.map(articlepost =>
+    const articleposts=posts.map(articlepost =>
         <Article key={articlepost.id}title={articlepost.title}
         date={articlepost.date}preview={articlepost.preview}/>)
     return(
-<main>{articles}</main>
+<main>{articleposts}</main>
     );
 }
 export default ArticleList;
