a4090b179f354b0c4a695e915833059a472a1db2
Mercy <mercycheruiyot2013@gmail.com>
2022-09-27 14:10:41 +0300

    Completed Assignment

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,20 @@
 import React from "react";
-function Article({title,date="January 1, 1970",preview}){
+
+function minutess(minutes){
+    const interval=minutes<30 ?5:10;
+    const emoji=minutes< 30 ? "-" : " "
+
+    let emojis="";
+    for (let i=0; i< minutes; i += interval){
+        emojis +=emoji;
+    } 
+  
+
+return emojis;
+}
+
+function Article({title,date="January 1, 1970",preview,minutes}){
+    const emojis = minutess(minutes);
     return(
 <article>
     <h3>{title}</h3>
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,8 @@ import Article from "./Article";
 function ArticleList({posts}){
     const articleposts=posts.map(articlepost =>
         <Article key={articlepost.id}title={articlepost.title}
-        date={articlepost.date}preview={articlepost.preview}/>)
+        date={articlepost.date}preview={articlepost.preview}
+        minutes={articlepost.minute}/>)
     return(
 <main>{articleposts}</main>
     );
